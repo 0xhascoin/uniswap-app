c33b22dcdd12ed175c73257378f70383bfe8a296
hasan <hasanelmi678@gmail.com>
2022-03-11 18:43:18 +0000

    added header

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/header';
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#00111c] text-white select-none flex flex-col justify-between`,
+
+}
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      App
+    <div className={style.wrapper}>
+      <Header />
+      <h2>Main</h2>
+      <h2>Transaction History</h2>
     </div>
   )
 }
